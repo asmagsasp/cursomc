d13512a89abdd8d359f4d38307bfd329fe2aec88
Abel S Magalhães <abel.souza.magalhaes@hotmail.com>
2018-07-08 21:44:37 -0400

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,30 @@
+package com.nelioalves.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao () {
+		
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+		
+	}
+
+}
